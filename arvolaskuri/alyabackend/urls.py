199017c6640416fc359ff928685ce272d322bba2
arinasold <arina.soldan@gmail.com>
2024-03-19 17:47:20 +0200

    Link for the pictures

--- arvolaskuri/alyabackend/urls.py
+++ arvolaskuri/alyabackend/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
 from alyabackend import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('instructions', views.InstructionsJson.as_view(), name="instructions"),
     path('sendpic', views.ReceivePic.as_view(), name='picture'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
